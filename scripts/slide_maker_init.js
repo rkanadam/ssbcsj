212aec235dda809451519d3e2187fcdde7b220f3
rkanadam <raghuram.kanadam@gmail.com>
2015-12-27 02:08:45 -0800

    Checking to see if slide maker will work

--- scripts/slide_maker_init.js
+++ scripts/slide_maker_init.js
@@ -2,7 +2,7 @@
     var script = document.createElement('script');
     script.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js');
     script.onload = function () {
-        jQuery.get("http://localhost/ssbcsj/slide_maker.php", {}, function (html) {
+        jQuery.get("slide_maker.php", {}, function (html) {
             document.open("text/html");
             document.write(html);
             document.close();
