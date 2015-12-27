3dc82ee9a020d7ae340252b60141860479d5fe38
rkanadam <raghuram.kanadam@gmail.com>
2015-12-27 02:10:38 -0800

    Checking to see if slide maker will work

--- slide_maker.php
+++ slide_maker.php
@@ -1,5 +1,14 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+// respond to preflights
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    // return only the headers and not the content
+    // only allow CORS if we're doing a GET - i.e. no saving for now.
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']) && $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'GET') {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Headers: X-Requested-With');
+    }
+    exit;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
