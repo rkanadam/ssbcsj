6effae71298b3e38a917dd8dd935e54c0d84f8eb
rkanadam <raghuram.kanadam@gmail.com>
2015-12-27 01:51:17 -0800

    Checking to see if slide maker will work

--- scripts/slide_maker_init.js
+++ scripts/slide_maker_init.js
@@ -2,7 +2,7 @@
     var script = document.createElement('script');
     script.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js');
     script.onload = function () {
-        jQuery.get("slide_maker.html", {}, function (html) {
+        jQuery.get("slide_maker.php", {}, function (html) {
             document.open("text/html");
             document.write(html);
             document.close();
