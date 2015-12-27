71946e07d659a58171e257915a7015f8d75cad94
rkanadam <raghuram.kanadam@gmail.com>
2015-12-27 02:21:18 -0800

    Checking to see if slide maker will work

--- scripts/slide_maker_init.js
+++ scripts/slide_maker_init.js
@@ -1,17 +1,11 @@
 (function () {
-    var script = document.createElement('script');
-    script.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js');
-    script.onload = function () {
-        $.ajax({
-            url: "slide_maker.php",
-            jsonp: "callback",
-            dataType: "jsonp",
-            success: function (html) {
-                document.open("text/html");
-                document.write(html);
-                document.close();
-            }
-        });
+    window.onSlideMakerLoad = function (html) {
+        document.open("text/html");
+        document.write(html);
+        document.close();
     };
+    var script = document.createElement('script');
+    script = document.createElement('script');
+    script.setAttribute('src', "slide_maker.php?callback=onSlideMakerLoad");
     document.head.appendChild(script);
 })();
