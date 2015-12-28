907b655aab541be284ecf7e9ae383f5d54c2304f
rkanadam <raghuram.kanadam@gmail.com>
2015-12-27 21:02:14 -0800

    Checking to see if slide maker will work

--- scripts/slide_maker.js
+++ scripts/slide_maker.js
@@ -9,55 +9,57 @@
             $("#indicator").hide();
         });
 
-        google.script.run
-            .withSuccessHandler(function (bhajans) {
-                bhajans.sort(function (b1, b2) {
-                    return b1[0].toLocaleLowerCase() - b2[0].toLocaleLowerCase();
-                });
-
-                bhajans = $.map(bhajans, function (b) {
-                    var bhajan = {};
-                    bhajan.firstLine = $.trim(b[0].split("\n")[0]);
-                    bhajan.lyrics = b[0];
-                    bhajan.meaning = b[1];
-                    return bhajan;
-                });
-
-                var bhajanHound = new Bloodhound({
-                    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('firstLine'),
-                    queryTokenizer: Bloodhound.tokenizers.whitespace,
-                    local: bhajans
-                });
-
-
-                bhajanHound.initialize();
-                $('#bhajanPicker').typeahead({
-                        hint: true,
-                        highlight: true,
-                        minLength: 1
-                    },
-                    {
-                        name: 'bhajanHound',
-                        displayKey: 'firstLine',
-                        limit: 15,
-                        source: bhajanHound
-                    }).on('typeahead:selected', function (e, bhajan) {
-                        $("#lyrics textarea")
-                            .val(bhajan.lyrics)
-                        $("#meaning textarea")
-                            .val(bhajan.meaning);
+        if (window.google) {
+            google.script.run
+                .withSuccessHandler(function (bhajans) {
+                    bhajans.sort(function (b1, b2) {
+                        return b1[0].toLocaleLowerCase() - b2[0].toLocaleLowerCase();
                     });
-                $("#bhajanPicker").parent(".twitter-typeahead").css("display", "block");
-            })
-            .withUserObject(this)
-            .getDevotionalSongs();
 
-        google.script.run
-            .withSuccessHandler(function (sheet) {
-                console.log (sheet);
-                sheet = window.parse_bhajans(sheet);
-                console.log ("After", sheet);
-            }).withUserObject(this)
-            .getCurrentSheet();
+                    bhajans = $.map(bhajans, function (b) {
+                        var bhajan = {};
+                        bhajan.firstLine = $.trim(b[0].split("\n")[0]);
+                        bhajan.lyrics = b[0];
+                        bhajan.meaning = b[1];
+                        return bhajan;
+                    });
+
+                    var bhajanHound = new Bloodhound({
+                        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('firstLine'),
+                        queryTokenizer: Bloodhound.tokenizers.whitespace,
+                        local: bhajans
+                    });
+
+
+                    bhajanHound.initialize();
+                    $('#bhajanPicker').typeahead({
+                            hint: true,
+                            highlight: true,
+                            minLength: 1
+                        },
+                        {
+                            name: 'bhajanHound',
+                            displayKey: 'firstLine',
+                            limit: 15,
+                            source: bhajanHound
+                        }).on('typeahead:selected', function (e, bhajan) {
+                            $("#lyrics textarea")
+                                .val(bhajan.lyrics)
+                            $("#meaning textarea")
+                                .val(bhajan.meaning);
+                        });
+                    $("#bhajanPicker").parent(".twitter-typeahead").css("display", "block");
+                })
+                .withUserObject(this)
+                .getDevotionalSongs();
+
+            google.script.run
+                .withSuccessHandler(function (sheet) {
+                    console.log(sheet);
+                    sheet = window.parse_bhajans(sheet);
+                    console.log("After", sheet);
+                }).withUserObject(this)
+                .getCurrentSheet();
+        }
     });
 })(jQuery);
