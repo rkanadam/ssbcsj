0ccb02630d5ee2afa2c2e8fa0c64b0f07ee8a5e6
rkanadam <raghuram.kanadam@gmail.com>
2021-08-12 00:33:34 +0530

    Fixing birthday bhajans sign up.

--- scripts/birthday_bhajan_signup.js
+++ scripts/birthday_bhajan_signup.js
@@ -1,113 +1,130 @@
-(function ($) {
-    $(function () {
+(function($) {
+  $(function() {
 
-        var daysToEvents = {};
-        var daysToDates = {};
-        var apiLocation = "../server/birthday_bhajan_signup.php";
-        $.get(apiLocation, function (events) {
-            events = events || [];
-            for (var i = 0, len = events.length; i < len; ++i) {
-                var event = events[i];
-                var start = new Date(event.start);
-                var day = start.getDay();
-                //If the event does not start at 5PM or 7:15 PM or 8PM then it must be a service event or something like that. Just ignore it
-                if (!(start.getHours() === 17 || start.getHours() === 20 || (start.getHours() === 19 && start.getMinutes() === 15))) {
-                    continue;
-                }
-                if (daysToEvents[day]) {
-                    daysToEvents[day].push(event);
-                    daysToDates[day].push(new Date(event.start).getTime());
-                } else {
-                    daysToEvents[day] = [event];
-                    daysToDates[day] = [new Date(event.start).getTime()];
-                }
-            }
-        }, "json");
+    var daysToEvents = {};
+    var daysToDates = {};
+    var apiLocation = '../server/birthday_bhajan_signup.php';
+    $.get(apiLocation, function(events) {
+      events = events || [];
+      for (var i = 0, len = events.length; i < len; ++i) {
+        var event = events[i];
+        var start = new Date(event.start);
+        //convert to PST
+        start = new Date(
+            start.toLocaleString('en-US', {timeZone: 'America/Los_Angeles'}));
+        var day = start.getDay();
+        //If the event does not start at 5PM or 7:15 PM or 8PM then it must be a service event or something like that. Just ignore it
+        if (!(start.getHours() === 17 || start.getHours() === 20 ||
+            start.getHours() === 19)) {
+          continue;
+        }
+        if (daysToEvents[day]) {
+          daysToEvents[day].push(event);
+          daysToDates[day].push(new Date(event.start).toLocaleString('en-US',
+              {timeZone: 'America/Los_Angeles'}));
+        } else {
+          daysToEvents[day] = [event];
+          daysToDates[day] = [
+            new Date(event.start).toLocaleString('en-US',
+                {timeZone: 'America/Los_Angeles'})];
+        }
+      }
+    }, 'json');
 
-        $('select').selectpicker();
+    $('select').selectpicker();
 
-        $("#city select").change(function () {
-            $("#date, #details").hide();
+    $('#city select').change(function() {
+      $('#date, #details').hide();
 
-            var $this = $(this);
-            var val = $this.val();
-            if (!val) {
-                $("#date select").empty().selectpicker('refresh');
-                $("#details:input").val("");
-                return;
-            }
+      var $this = $(this);
+      var val = $this.val();
+      if (!val) {
+        $('#date select').empty().selectpicker('refresh');
+        $('#details:input').val('');
+        return;
+      }
 
-            var daysOfTheWeek = $.map(val.split(",", -1), function (v) {
-                return parseInt($.trim(v), 10);
-            });
+      var daysOfTheWeek = $.map(val.split(',', -1), function(v) {
+        return parseInt($.trim(v), 10);
+      });
 
-            var availableDates = [];
-            $.each(daysOfTheWeek, function (_, dayOfTheWeek) {
-                var firstDate = new Date(2021, 7, 20, 19, 15, 0, 0); // This should be the first day the bhajan starts
-                var lastDate = new Date(2021, 10, 22, 19, 0, 0, 0); // This should be the last day the bhajan starts
-                var d = new Date(firstDate.toISOString());
-                d.setDate(d.getDate() - d.getDay() + dayOfTheWeek);
-                //We are trying to determine the very first day of the said week of the day when the bhajan will begin
-                //So let us say the bhajan starts on a Wednesday then the first monday will be after that date
-                if (d.getTime() < firstDate.getTime()) {
-                    d.setDate(d.getDate() + 7);
-                }
+      var availableDates = [];
+      $.each(daysOfTheWeek, function(_, dayOfTheWeek) {
+        var firstDate = new Date(2021, 7, 20, 19, 0, 0, 0); // This should be the first day the bhajan starts
+        var lastDate = new Date(2021, 10, 22, 19, 0, 0, 0); // This should be the last day the bhajan starts
+        var d = new Date(firstDate.getTime());
+        d.setDate(d.getDate() - d.getDay() + dayOfTheWeek);
+        //We are trying to determine the very first day of the said week of the day when the bhajan will begin
+        //So let us say the bhajan starts on a Wednesday then the first monday will be after that date
+        if (d.getTime() < firstDate.getTime()) {
+          d.setDate(d.getDate() + 7);
+        }
 
-                var reservedDates = daysToDates[dayOfTheWeek] || [];
-                for (var i = 0; i < 14; ++i) {
-                    if (reservedDates.indexOf(d.getTime()) === -1 && d.getTime() <= lastDate.getTime()) {
-                        availableDates.push(d.getTime());
-                    }
-                    d.setDate(d.getDate() + 7);
-                }
-            });
-            availableDates.sort ();
-            var html = $(availableDates).map(function () {
-                var d = new Date();
-                d.setTime(this);
-                return ["<option value=\"", d.toISOString(), "\">", d.toDateString(), "</option>"]
-            }).get().join("");
-            $("#date select").html("<option value = \"\"></option>" + html);
-            $("#date select").selectpicker('refresh');
-            $("#date").show();
-        });
-
-        $("#date select").change(function () {
-            var $this = $(this);
-            var val = $this.val();
-            if (!val) {
-                $("#details").hide();
-                $("#details:input").val("");
-                return;
-            }
-            $("#details").show();
-        });
-
-        $("#bhajanSignupForm").validator().on("submit", function (e) {
-            if (e.isDefaultPrevented()) {
-                return;
-            }
-
-            e.preventDefault();
-            var $this = $(this);
-            $("#city [name='cityName']").val ($("#city select option:selected").text ());
-            $.post(apiLocation, $this.serialize(), function (response) {
-                if (response !== true) {
-                    alert("Uh-oh we had some trouble requesting your signup. Please contact the web master.");
-                } else {
-                    alert("We have requested for your signup. You will receive a confirmation  e-mail soon! If not see Raghuram @ raghuram.kanadam@gmail.com");
-                }
-                window.location.reload();
-            }, "json");
-
-        });
-
-        $(document).ajaxStart(function () {
-            window.scrollTo(0, 0);
-            $("#indicator").show();
-        });
-        $(document).ajaxStop(function () {
-            $("#indicator").hide();
-        });
+        var reservedDates = daysToDates[dayOfTheWeek] || [];
+        for (var i = 0; i < 14; ++i) {
+          const formattedString = d.toLocaleString('en-US');
+          if (reservedDates.indexOf(formattedString) === -1 && d.getTime() <=
+              lastDate.getTime()) {
+            availableDates.push(formattedString);
+          }
+          d.setDate(d.getDate() + 7);
+        }
+      });
+      availableDates.sort((a1, a2) => {
+        return new Date (a1).getTime() - new Date(a2).getTime();
+      });
+      var html = $(availableDates).map(function() {
+        return [
+          '<option value="',
+          this,
+          '">',
+          new Date(this).toDateString(),
+          '</option>'];
+      }).get().join('');
+      $('#date select').html('<option value = ""></option>' + html);
+      $('#date select').selectpicker('refresh');
+      $('#date').show();
     });
+
+    $('#date select').change(function() {
+      var $this = $(this);
+      var val = $this.val();
+      if (!val) {
+        $('#details').hide();
+        $('#details:input').val('');
+        return;
+      }
+      $('#details').show();
+    });
+
+    $('#bhajanSignupForm').validator().on('submit', function(e) {
+      if (e.isDefaultPrevented()) {
+        return;
+      }
+
+      e.preventDefault();
+      var $this = $(this);
+      $('#city [name=\'cityName\']').
+          val($('#city select option:selected').text());
+      $.post(apiLocation, $this.serialize(), function(response) {
+        if (response !== true) {
+          alert(
+              'Uh-oh we had some trouble requesting your signup. Please contact the web master.');
+        } else {
+          alert(
+              'We have requested for your signup. You will receive a confirmation  e-mail soon! If not see Raghuram @ raghuram.kanadam@gmail.com');
+        }
+        window.location.reload();
+      }, 'json');
+
+    });
+
+    $(document).ajaxStart(function() {
+      window.scrollTo(0, 0);
+      $('#indicator').show();
+    });
+    $(document).ajaxStop(function() {
+      $('#indicator').hide();
+    });
+  });
 })(jQuery);
--- server/birthday_bhajan_signup.php
+++ server/birthday_bhajan_signup.php
@@ -10,7 +10,7 @@ if ($method === "get") {
     $optParams = array(
         'orderBy' => 'startTime',
         'singleEvents' => TRUE,
-        'timeMin' => '2020-08-21T02:00:00.000Z'
+        'timeMin' => '2021-08-21T02:00:00.000Z'
     );
 
     $results = $calendarService->events->listEvents($calendarId, $optParams);
