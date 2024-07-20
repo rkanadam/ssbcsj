ea9aeacf48f7ab708a90fac654d176ae3f5b2ef2
rkanadam <raghuram.kanadam@gmail.com>
2024-07-19 22:47:59 -0700

    Updating birthday bhajan signup page

--- scripts/birthday_bhajan_signup.js
+++ scripts/birthday_bhajan_signup.js
@@ -2,102 +2,96 @@
     $(function () {
         var allEvents = [];
         var reservedDates = [];
-        var apiLocation = 'https://slides.ssbcsj.org/sssbcsj_api/birthday/signups';
-        $.get(apiLocation, function (events) {
-            events = events || [];
-            for (var i = 0, len = events.length; i < len; ++i) {
-                var event = events[i];
-                var start = new Date(event.start.dateTime);
-                //convert to PST
-                start = new Date(
-                    start.toLocaleString('en-US', {timeZone: 'America/Los_Angeles'}));
-
-                //If the event does not start at 5PM or 7:30 PM or 8PM then it must be a service event or something like that. Just ignore it
-                if (!(start.getHours() === 17 || start.getHours() === 20 ||
-                    start.getHours() === 19)) {
-                    continue;
-                }
-                reservedDates.push(start.getTime());
+        // https://docs.google.com/spreadsheets/d/1W3DK8ySOsPGZDgCBQQRmc9KRO8C1OnEqSRnzrpXLOaA/edit?gid=87825228#gid=87825228
+        var apiLocation = 'https://script.google.com/macros/s/AKfycbzT4Ee9ljsl5a2VkDzh68s77u8oOb8_6Pfk_jh5wXuJt-CGIINPlwKkMQSq2xoHhTWL/exec';
+        $.get(apiLocation, {"path": "areas"}, function (areas) {
+            const areaSet = new Set();
+            areas.forEach((area, index) => {
+                areaSet.add(area);
+            })
+            const cities = Array.from(areaSet.values()).map(area => {
+                return `<option value = "${area}">${area}</option>`
+            }).join("")
+            $("#city select").html(`<option value = ""></option>${cities}`);
+            $('#city select').selectpicker('refresh');
+        });
+        $("#city select").on("change", function () {
+            var $this = $(this);
+            var val = $this.val();
+            if (val) {
+                console.log("City", val, "selected");
             }
-            allEvents = events;
-
-            //Swami's birth year
-            const swamisBirthday = new Date().getFullYear() - 1926 + 1;
-
-            var availableDates = [];
-            var firstDate = new Date(2023, 7, 18, 19, 30, 0, 0); // This should be the first day the bhajan starts
-            //firstDate.setDate(firstDate.getDate() - swamisBirthday + 1);
-            var lastDate = new Date(2023, 10, 22, 19, 30, 0, 0); // This should be the last day the bhajan starts
-            var d = new Date(firstDate.getTime());
-            for (var i = 1; i <= 98; ++i) {
-                if (reservedDates.indexOf(d.getTime()) === -1 && d.getTime() <=
-                    lastDate.getTime()) {
-                    availableDates.push({
-                        day: i,
-                        date: d.getTime()
-                    });
-                }
-                d.setDate(d.getDate() + 1);
-            }
-            var html = $(availableDates).map(function () {
-                const d = new Date(this.date);
-                return [
-                    '<option value="',
-                    d.toISOString(),
-                    '">',
-                    `${d.toDateString()} (day #${this.day})`,
-                    '</option>'];
-            }).get().join('');
-            $('#date select').html('<option value = ""></option>' + html);
-            $('#date select').selectpicker('refresh');
-            $('#date').show();
-            $('#date select').on("change", function () {
-                var $this = $(this);
-                var val = $this.val();
-                if (!val) {
-                    $('#details').hide();
-                    $('#details:input').val('');
-                    return;
-                }
-                $('#details').show();
-
-                $('#bhajanSignupForm').validator().on('submit', function (e) {
-                    if (e.isDefaultPrevented()) {
+            $.get(apiLocation, {"path": "signups", area: val}, function (availableDates) {
+                var html = availableDates.map(function ({date, value}, index) {
+                    return [
+                        '<option value="',
+                        value,
+                        '">',
+                        date,
+                        '</option>'].join("");
+                }).join('');
+                $('#date select').html('<option value = ""></option>' + html);
+                $('#date select').selectpicker('refresh');
+                $('#date').show();
+                $('#date select').on("change", function () {
+                    var $this = $(this);
+                    var val = $this.val();
+                    if (!val) {
+                        $('#details').hide();
+                        $('#details:input').val('');
                         return;
                     }
+                    $('#details').show();
 
-                    e.preventDefault();
-                    var $this = $(this);
+                    $('#bhajanSignupForm').validator().on('submit', function (e) {
+                        if (e.isDefaultPrevented()) {
+                            return;
+                        }
 
-                    function getFormData($form) {
-                        var unindexed_array = $form.serializeArray();
-                        var indexed_array = {};
+                        e.preventDefault();
+                        var $this = $(this);
 
-                        $.map(unindexed_array, function (n, i) {
-                            indexed_array[n['name']] = n['value'];
-                        });
+                        function getFormData($form) {
+                            var unindexed_array = $form.serializeArray();
+                            var indexed_array = {};
 
-                        return indexed_array;
-                    }
+                            $.map(unindexed_array, function (n, i) {
+                                indexed_array[n['name']] = n['value'];
+                            });
 
-                    $.post(apiLocation, getFormData($this), function (response) {
-                        alert(
-                            'We have requested for your signup. You will receive a confirmation  e-mail soon! If not see DC @ ssbcsj.devotion@gmail.com or 408-667-9448');
-                        window.location.reload();
-                    }, 'json');
+                            return indexed_array;
+                        }
 
+                        console.log(getFormData($this));
+
+                        fetch(apiLocation, {
+                            method: "POST",
+                            mode: "no-cors",
+                            redirect: "follow",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify(getFormData($this)),
+                        })
+                            .then((response) => response.text())
+                            .then((result) => {
+                                alert(
+                                    'We have requested for your signup. You will receive a confirmation  e-mail soon! If not see DC @ ssbcsj.devotion@gmail.com or 925-642-2680');
+                                window.location.reload();
+                            });
+                    });
+
+                    $(document).ajaxStart(function () {
+                        window.scrollTo(0, 0);
+                        $('#indicator').show();
+                    });
+                    $(document).ajaxStop(function () {
+                        $('#indicator').hide();
+                    });
                 });
 
-                $(document).ajaxStart(function () {
-                    window.scrollTo(0, 0);
-                    $('#indicator').show();
-                });
-                $(document).ajaxStop(function () {
-                    $('#indicator').hide();
-                });
-            });
-
-        }, 'json');
+            }, 'json');
+        });
 
         $('select').selectpicker();
 
